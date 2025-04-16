5f016f1cfc32d7b31b6633a6acb0d02f8d8cb590
Aleksandra Borowska <olkaborowska333@wp.pl>
2025-04-16 21:38:59 +0200

    work on login functionality for users

--- UI/ui/src/Login.js
+++ UI/ui/src/Login.js
@@ -1,16 +1,35 @@
 import './Login.css';
+import { useState } from "react"
+import axios from "axios"
 
 function Login(){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
+    async function handleLogIn(e){
+        e.preventDefault();
+        const requestBody = {email, password}
+        console.log('You clicked submit.')
+        console.log(requestBody)
+        const response = await axios.post('http://127.0.0.1:5000/login', requestBody, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
     return(
         <div className='login-card'>
             <h1 className='login-headline'>Habit Tracker</h1>
-            <form className='login-form'>
-                <label className='label'>Enter address:</label>
-                <input className='input' type="email" />
-                <label className='label'>Password:</label>
-                <input className='input' type="password" />
-                <button className='login-button'>Log In</button>
+            <form className='login-form' onSubmit={handleLogIn}>
+                <div className='login-input-conatiner'>
+                    <label className='label'>Enter address:</label>
+                    <input className='input' type="email" onChange={e=>setEmail(e.target.value)}/>
+                </div>
+                <div className='login-input-conatiner'>
+                    <label className='label'>Password:</label>
+                    <input className='input' type="password"  onChange={e=>setPassword(e.target.value)}/>
+                </div>
+                <button className='login-button' type='submit'>Log In</button>
                 <p className='login-p'>Don't have an account? 
                     <a href='#' className='login-sign-up-link'>
                         Sign up
